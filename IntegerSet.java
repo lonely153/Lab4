b260e79308496970a80de47d1fd8c68aecb2fe6c
lonely153 <chamikon.k@ku.th>
2025-07-17 19:03:21 +0700

    Add files via upload

---
+++ IntegerSet.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+/** ADT ที่เก็บเลขจำนวนเต็มใด ๆ เรียงจากน้อยไปมาก ห้ามมี null หรือตัวเลขซ้ำ
+ * 
+ */
+
+public class IntegerSet {
+    ArrayList<Integer> Numbers;
+
+    /**
+     * สร้างเซตของเลขด้วย ArrayList
+     */
+    public IntegerSet(){
+        Numbers = new ArrayList<>();
+        CheckRep();
+    }
+    /**
+     * ตรวจสอบว่า Rep Invariant เป็นจริงหรือไม่
+     */
+    private void CheckRep(){
+        int n = Numbers.size();
+        for (int i = 0; i < n-1; i++) {
+            if (Numbers.get(i) >= Numbers.get(i+1)) {
+                throw new RuntimeException("Rep invariant violated!");
+            }
+        }
+    }
+    /**
+     * เพิ่มเลขลงในเซต
+     * @param x เลขที่ต้องการเพิ่ม
+     */
+    public void add(Integer x){
+        Numbers.add(x);
+        CheckRep();
+    }
+    /**
+     * ลบเลขออกในเซต
+     * @param x เลขที่ต้องการลบ
+     */
+    public void remove(Integer x){
+        Numbers.remove(x);
+        CheckRep();
+    }
+
+    /**
+     * ตรวจสอบว่ามีเลขนี้อยู่ในเซตหรือไม่
+     * @param x เลขที่ต้องการตรวจสอบ
+     * @return true หากมี x อยู่ในเซต, false หากไม่มี
+     */
+    public boolean contains(Integer x) {
+        return Numbers.contains(x);
+    }
+
+    /**
+     * คืนค่าขนาดของเซต
+     * @return จำนวนสมาชิกในเซต Numbers
+     */
+    public int size() {
+        return Numbers.size();
+    }
+
+    /**
+     * คืนค่าเซตเป็น String
+     * @return String ของ Numbers
+     */
+    public String toString(){
+        return Numbers.toString();
+    }
+}
---
+++ IntegerSetTest.java
@@ -0,0 +1,70 @@
+public class IntegerSetTest {
+    public static void main(String[] args){
+        System.out.println("--- Starting CharSet Manual Tests ---");
+        int passed = 0;
+        int failed = 0;
+
+        // Test 1: การสร้าง, เพิ่มข้อมูล
+        System.out.println("\n--- Testing add() and Creating ---");
+        IntegerSet set1 = new IntegerSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        String expected1 = "[1, 2, 3]";
+        if (set1.toString().equals(expected1)) {
+            System.out.println("PASSED: Add and Creating works correctly.");
+            passed++;
+        } else {
+            System.out.println("FAILED: Expected " + expected1 + " but got " + set1.toString());
+            failed++;
+        }
+
+        //Test 2: ลบ
+        System.out.println("\n--- Testing Removing ---");
+        set1.remove(1);
+        String expected2 = "[2, 3]";
+        if (set1.toString().equals(expected2)) {
+            System.out.println("PASSED: Removing works correctly.");
+            passed++;
+        } else {
+            System.out.println("FAILED: Expected " + expected2 + " but got " + set1.toString());
+            failed++;
+        }
+
+        //Test 3: ตัวซ้ำ
+        System.out.println("\n--- Testing Dup ---");
+        set1.add(3);
+        String expected3 = "[2, 3]";
+        if (set1.toString().equals(expected3)) {
+            System.out.println("PASSED: Dup works correctly.");
+            passed++;
+        } else {
+            System.out.println("FAILED: Expected " + expected3 + " but got " + set1.toString());
+            failed++;
+        }
+        //Test 4: ตรวจสอบ
+        System.out.println("\n--- Checking ---");
+        set1.add(3);
+        String expected4 = "[2, 3]";
+        if () {
+            System.out.println("PASSED: Dup works correctly.");
+            passed++;
+        } else {
+            System.out.println("FAILED: Expected " + expected4 + " but got " + set1.toString());
+            failed++;
+        }
+        
+
+        // --- สรุปผล ---
+        System.out.println("\n--------------------");
+        System.out.println("--- Test Summary ---");
+        System.out.println("Passed: " + passed + ", Failed: " + failed);
+        if (failed == 0) {
+            System.out.println("Excellent! All tests passed!");
+        } else {
+            System.out.println("Some tests failed.");
+        }
+
+        
+    }
+}
